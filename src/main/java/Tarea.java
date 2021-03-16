8eede8b769197fd15fbeb6c7cf945d7afcb02067
Tomate77 <aguerofeli21@gmail.com>
2021-03-16 11:52:27 +0100

    commit10

--- src/main/java/Tarea.java
+++ src/main/java/Tarea.java
@@ -2,11 +2,12 @@ import java.util.List;
 
 public class Tarea {
     String titulo;
-    String descripcion;
+    String descripcion; //permite NULL
     List<Persona> listaPersonas;
     Persona responsable;
     int prioridad;
     String fechaCreado;
+
     String fechaFin;
     boolean isFinalizada;
     Resultado resultado;
@@ -16,14 +17,28 @@ public class Tarea {
 
     public Tarea(String titulo) {
         this.titulo = titulo;
+        //set fecha creacion
     }
     public void marcarComoFinalizada(){
         isFinalizada = true;
+        //set fecha fin
     }
     public void addPersona(Persona persona) {
         listaPersonas.add(persona);
     }
     public void removePersona(Persona persona) {
         listaPersonas.remove(persona);
+    }
+
+    public void setResponsable(Persona responsable) {
+        this.responsable = responsable;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
     }
 }
