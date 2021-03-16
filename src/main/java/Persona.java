5a97ce210023d164847a3e1d82fab5add52c718d
Tomate77 <aguerofeli21@gmail.com>
2021-03-16 11:05:56 +0100

    commit2

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -1,16 +1,20 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Persona {
     private String nombre;
     private String correo;
-    private List<Tarea> tareas;
+    private List<Tarea> tareas; //tareas de las que es responsable esta persona
 
     public Persona(String nombre, String correo) {
         this.nombre = nombre;
         this.correo = correo;
+        tareas = new ArrayList<>();
     }
 
     public String getNombre(){return nombre;}
     public String getCorreo(){return correo;}
     public List<Tarea> getTareas(){return tareas;}
+
+    //fuction add persona
 }
