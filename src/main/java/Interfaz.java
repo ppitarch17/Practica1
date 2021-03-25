fd1d9f04dab26797084b33777cbf2b4b83a39b5a
Aur77 <agilax77@gmail.com>
2021-03-25 23:16:38 +0100

    commit 10

--- src/main/java/Interfaz.java
+++ src/main/java/Interfaz.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Interfaz {
 
@@ -28,23 +29,42 @@ public class Interfaz {
     }
 
     public static void ShowMenu(){
+        System.out.println("Selecciona: ");
         System.out.println("1 Crear Persona");
         System.out.println("2 Crear Tarea");
         System.out.println("3 añadir persona a tarea");
         System.out.println("4 finalizar tarea");
         System.out.println("5 listar personas");
         System.out.println("6 listar tareas");
+        Scanner scanner = new Scanner(System.in);
+        int op = scanner.nextInt();
 
+        switch (op){
+            case 1:
+                op1();
+                break;
+            case 2:
+                op2();
+                break;
+            default:
+                error();
+                break;
+        }
         //recibir num
         //verificar que el numero
     }
 
-    public static Proyecto createProyect(){
-
+    public static void op1(){
+        System.out.println("op 1");
     }
 
+    public static void op2(){
+        System.out.println("op 2");
+    }
+
+
     public static void error(){
-        System.out.println("Error: ");
+        System.out.println("Error: -------");
         ShowMenu();
     }
 
